c530ec41fa721400b56ac7723307d6d904db239c
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-03-09 10:03:54 -0300

    add: ionic

---
+++ www/js/app.js
@@ -0,0 +1,57 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.about', {
+    url: '/about',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/about.html'
+      }
+    }
+  })
+
+  .state('app.articles', {
+    url: '/articles',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/articles.html'
+      }
+    }
+  })
+
+  .state('app.projects', {
+    url: '/projects',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/projects.html'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/about');
+});
